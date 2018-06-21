2127a74291497b4334b1ba5c50c7bb413ff7cfe3
Kyle Reed <reedk10@gmail.com>
2018-06-20 21:48:32 -0400

    First commit for prework

---
+++ ReedJavascriptExercises/scripts.js
@@ -0,0 +1,19 @@
+<script>
+
+//Exercise Four
+
+Var answer
+Prompt ("what time of the day is it?");
+Var answer = prompt ("what time of the day is it");
+document.write(answer);
+
+Function myFunction() {
+  Var greeting;
+  if () {
+   greeting = "Morning, you should eat breakfast. We suggest eggs and toast";
+} else if () {
+   greeting = "noon, you should eat lunch. We suggest a salad";
+} else () {
+   greeting = "Evening, you should et dinner. We suggest chicken and rice";
+
+</script>
